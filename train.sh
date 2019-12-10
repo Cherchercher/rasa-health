8161943563e605f3dbbdd8af1fa640dcaf3a97c2
akingbadeayobami <akinayosamtom@gmail.com>
2019-12-10 17:48:23 +0100

    Parameterize build and deploy #closes CAR-2799

--- train.sh
+++ train.sh
@@ -1,27 +1,13 @@
 #! /bin/bash
+
+agent=$1
+
 cd /Linguist
 
 python -m rasa_nlu.train \
-    -c /Linguist/configs/en/rasa_config.yml \
-    --data /Linguist/core/en/training_data.json \
-    --fixed_model_name core_en \
+    -c /Linguist/configs/$agent/rasa_config.yml \
+    --data /Linguist/core/$agent/training_data.json \
+    --fixed_model_name core_$agent \
     -o models \
     --project linguist \
     --verbose
-
-# uncomment for other agents
-# python -m rasa_nlu.train \
-#     -c /Linguist/configs/cn/rasa_config.yml \
-#     --data /Linguist/core/cn/training_data.json \
-#     --fixed_model_name core_cn \
-#     -o models \
-#     --project linguist \
-#     --verbose
-
-# python -m rasa_nlu.train \
-#     -c /Linguist/configs/de/rasa_config.yml \
-#     --data /Linguist/core/de/training_data.json \
-#     --fixed_model_name core_de \
-#     -o models \
-#     --project linguist \
-#     --verbose
