a290790b553d15be470098f0ba97f42f3a7b066f
Paul Schifferer <paul@carlabs.com>
2019-12-11 14:47:48 -0800

    Add custom replacement for agent and version in deployment manifest, since "kustomize edit set imagetag" isn't working for this case.

--- deploy-eks.sh
+++ deploy-eks.sh
@@ -20,9 +20,7 @@ cd kubernetes
 # strip agent from end of version string
 actual_version=$(echo $version | sed -e "s/-${agent}\$//")
 
-pushd base
-/usr/local/bin/kustomize edit set imagetag 261695625069.dkr.ecr.us-east-1.amazonaws.com/linguist-rasa-${agent}:${actual_version}
-popd
-
 ns=$(aws --region ${region} ssm get-parameter --name /rasa/linguistbot/${env}/${agent}/namespace --query 'Parameter.Value' --output text)
-/usr/local/bin/kustomize build overlays/${region}/${env}/${agent} | /usr/local/bin/kubectl --kubeconfig /var/lib/jenkins/.kube/${env}--${region}.config --namespace ${ns} apply -f -
+/usr/local/bin/kustomize build overlays/${region}/${env}/${agent} | \
+    sed -e "s/{{AGENT}}/${agent}/" -e "s/{{VERSION}}/${actual_version}/" | \
+    /usr/local/bin/kubectl --kubeconfig /var/lib/jenkins/.kube/${env}--${region}.config --namespace ${ns} apply -f -
