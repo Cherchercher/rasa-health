a4e47c640d184918baf6d5a6d0a40f100043fb30
Cherchercher <cher@carlabs.com>
2019-12-10 17:46:36 -0800

    attemp to install required packages

--- train.sh
+++ train.sh
@@ -4,8 +4,16 @@ agent=$1
 
 cd /Linguist
 
-RUN pip install -r requirements.txt
-RUN python -m spacy download de_trf_bertbasecased_lg
+if [ agent == 'de' ]
+    then
+        RUN pip install -r requirements_de.txt
+        RUN python -m spacy download de_trf_bertbasecased_lg
+fi
+
+if [ agent == 'cn' ]
+    then
+        RUN pip install -r requirements_cn.txt
+fi
 
 python -m rasa_nlu.train \
     -c /Linguist/configs/$agent/rasa_config.yml \
