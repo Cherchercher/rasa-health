534f1bbede669f698d1b42cee1eecb6a9f5bff04
Paul Schifferer <paul@carlabs.com>
2019-12-11 14:05:23 -0800

    Add agent to SSM parameter name for namespace lookup.

--- deploy-eks.sh
+++ deploy-eks.sh
@@ -24,5 +24,5 @@ pushd base
 /usr/local/bin/kustomize edit set imagetag 261695625069.dkr.ecr.us-east-1.amazonaws.com/linguist-rasa-${agent}:${actual_version}
 popd
 
-ns=$(aws --region ${region} ssm get-parameter --name /rasa/linguistbot/${env}/namespace --query 'Parameter.Value' --output text)
+ns=$(aws --region ${region} ssm get-parameter --name /rasa/linguistbot/${env}/${agent}/namespace --query 'Parameter.Value' --output text)
 /usr/local/bin/kustomize build overlays/${region}/${env}/${agent} | /usr/local/bin/kubectl --kubeconfig /var/lib/jenkins/.kube/${env}--${region}.config --namespace ${ns} apply -f -
