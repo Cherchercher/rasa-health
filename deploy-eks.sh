0f7f6828a0f84bea93cae08849ff97c0ee5deacb
Paul Schifferer <paul@carlabs.com>
2019-12-11 12:58:35 -0800

    Updates to build and deployment setup.

---
+++ deploy-eks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+set -e
+set -o pipefail
+
+agent=$1
+env=$2
+version=$3
+region=${4:-us-east-1}
+
+if [ -z "$agent" ] || [ -z "$env" ] || [ -z "$version" ]; then
+    echo "Usage: $0 <agent> <env> <version> [<region>]"
+    echo "          <region> defaults to 'us-east-1' if not specified."
+    exit 1
+fi
+
+cd kubernetes
+
+pushd base
+/usr/local/bin/kustomize edit set imagetag 261695625069.dkr.ecr.us-east-1.amazonaws.com/linguist-rasa-$agent:$version
+popd
+
+ns=$(aws --region $region ssm get-parameter --name /rasa/linguistbot/$env/namespace --query 'Parameter.Value' --output text)
+/usr/local/bin/kustomize build overlays/$region/$env | /usr/local/bin/kubectl --kubeconfig /var/lib/jenkins/.kube/${env}--${region}.config --namespace $ns apply -f -
--- train.sh
+++ train.sh
@@ -1,19 +1,14 @@
-#! /bin/bash
+#!/bin/bash
+
+set -x
+set -e
+set -o pipefail
 
 agent=$1
 
 cd /Linguist
 
-if [ $1 == 'de' ]
-    then
-        pip install -r requirements_de.txt
-        python -m spacy download de_trf_bertbasecased_lg
-fi
-
-if [ $1 == 'cn' ]
-    then
-        pip install -r requirements_cn.txt
-fi
+scripts/$agent/pre-train.sh
 
 python -m rasa_nlu.train \
     -c /Linguist/configs/$agent/rasa_config.yml \
