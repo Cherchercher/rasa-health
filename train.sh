9f48e30ef9f3dbf0e7995733151a7a3c0e976e0e
akingbadeayobami <akinayosamtom@gmail.com>
2019-12-16 18:40:16 +0100

    new rasa construct v2

--- train.sh
+++ train.sh
@@ -8,10 +8,9 @@ agent=$1
 
 cd /Linguist
 
-python -m rasa train \
+python -m rasa train nlu \
     --config /Linguist/configs/$agent/rasa_config.yml \
     --data /Linguist/core/$agent/training_data.json \
     --fixed-model-name linguist/core_$agent \
     --out models \
     --verbose
-    nlu
