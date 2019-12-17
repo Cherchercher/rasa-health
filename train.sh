c93c6dc9fc2c4d7bafcbfb0733d28b236d89ffb4
Cherchercher <cher@carlabs.com>
2019-12-16 17:17:22 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -7,11 +7,10 @@ set -o pipefail
 agent=$1
 
 cd /Linguist
-
-python -m rasa train \
-    --config /Linguist/configs/$agent/rasa_config.yml \
-    --data /Linguist/core/$agent/training_data.json \
-    --fixed-model-name linguist/core_$agent \
+ 
+python -m rasa train nlu \
+    --config ./configs/$agent/rasa_config.yml \
+    --data ./core/$agent/training_data.json \
+    --fixed-model-name core_$agent \
     --out models \
-    --verbose \
-    nlu
+    --verbose
