e9c6ad849803f173b513c8dc554e02f52f777158
Cherchercher <cher@carlabs.com>
2019-12-23 09:45:15 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -16,11 +16,11 @@ then
     --fixed-model-name core_$agent \
     --out ./tmp/tmpoyzgq_93/nlu/addons_intent_classifier_textcnn_tf \
     --verbose
+else
+    rasa train nlu \
+        --config ./configs/$agent/rasa_config.yml \
+        --nlu ./core/$agent/training_data.json \
+        --fixed-model-name core_$agent \
+        --out models \
+        --verbose
 fi
-
-rasa train nlu \
-    --config ./configs/$agent/rasa_config.yml \
-    --nlu ./core/$agent/training_data.json \
-    --fixed-model-name core_$agent \
-    --out models \
-    --verbose
