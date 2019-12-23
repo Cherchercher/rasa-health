50eb10433ef94e12dc723f1829d97b33b0ffd64a
Cherchercher <cher@carlabs.com>
2019-12-23 09:43:47 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -8,13 +8,13 @@ agent=$1
 
 cd /Linguist
 
-if [ agent == 'cn' ]
+if [ agent = 'cn' ]
 then
     rasa train nlu \
     --config ./configs/$agent/rasa_config.yml \
     --nlu ./core/$agent/training_data.json \
     --fixed-model-name core_$agent \
-    --out /tmp/tmpoyzgq_93/nlu/addons_intent_classifier_textcnn_tf \
+    --out ./tmp/tmpoyzgq_93/nlu/addons_intent_classifier_textcnn_tf \
     --verbose
 fi
 
