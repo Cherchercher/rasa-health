4e27fe144d03988a4979f6ec796b89e571927d87
akingbadeayobami <akinayosamtom@gmail.com>
2019-12-23 19:13:45 +0100

    Dynamic Probe

--- readiness-probe.sh
+++ readiness-probe.sh
@@ -6,7 +6,8 @@ CHECK_CORE_DE=$(echo $STATUS_JSON | jq '.model_file | index("core_de")')
 CHECK_CORE_CN=$(echo $STATUS_JSON | jq '.model_file | index("core_cn")')
 
 check_query() {
-    QUERY_JSON=$(curl -s --max-time 5 "http://localhost:5000/model/parse?emulation_mode=DIALOGFLOW" -X POST -d '{"text":"is the car available now"}')
+    QUERY_JSON=$(curl -s --max-time 5 "http://localhost:5000/model/parse?emulation_mode=DIALOGFLOW" -X POST -d "{\"text\":\"$1\"}")
+    echo $QUERY_JSON
     CHECK_QUERY=$(echo $QUERY_JSON | jq -r '.id')
 
     if [ "$CHECK_QUERY" == "null" ]; then
@@ -15,13 +16,13 @@ check_query() {
 }
 
 if [ "$CHECK_CORE_EN" != "null" ]; then
-    check_query '{"text":"is the car available now"}'
+    check_query "is the car available now"
 fi
 
 if [ "$CHECK_CORE_CN" != "null" ]; then
-    check_query "{\"text\":\"车\"}"
+    check_query "车"
 fi
 
 if [ "$CHECK_CORE_DE" != "null" ]; then
-    check_query "{\"text\":\"wie viel kostet\"}"
+    check_query "wie viel kostet"
 fi
