b1559db2f9da75d5863511d9c96bd011d165670a
Cherchercher <cher@carlabs.com>
2020-02-05 11:13:33 -0800

    refactor with testDrive

--- tests/en/test.sh
+++ tests/en/test.sh
@@ -1,1 +1,1 @@
-[ -d results ]&& rm -rf results; rasa test nlu -u ./data/ground_truth_data.json  --model models; [[ -s ./results/intent_errors.json ]] && echo 'Error: not all phrases got matched with right intents'|| echo 'success'
+[ -d results ]&& rm -rf results; rasa test nlu -u ./core/en/ --config ./configs/en/rasa_config.yml --cross-validation --model models; [[ -s ./results/intent_errors.json ]] && echo 'Error: not all phrases got matched with right intents'|| echo 'success'
