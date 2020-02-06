1dd62acf8e72122e99c13201640d0e290cd65643
Storm <storm@Storms-MacBook-Pro-2.local>
2020-02-05 16:59:20 -0800

    added orders intents, and ground truth, etc

---
+++ tests/en/test-ground_truth.sh
@@ -0,0 +1,1 @@
+[ -d results ]&& rm -rf results; rasa test nlu -u ./data/ground_truth_data_en.json  --model models; [[ -s ./results/intent_errors.json ]] && echo 'Error: not all phrases got matched with right intents'|| echo 'success'
